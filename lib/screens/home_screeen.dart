2c0a9a612abaa25444d72d074c068fdb39e01651
Parth <parthrathod625@gmail.com>
2024-06-11 19:16:59 +0530

    First catalog app commit

---
+++ lib/screens/home_screeen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreeen extends StatelessWidget {
+  const HomeScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: const Center(
+        child: Text("Welcome to Catalog App"),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
